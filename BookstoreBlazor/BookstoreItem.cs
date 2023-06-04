ecaea6959dd8e68e803e8c9c564b9c18cc494182
jforgaritenzoo <forgaritenzo6@gmail.com>
2023-06-04 21:41:45 +0700

    Pertemuan 12: Webclient API

---
+++ BookstoreBlazor/BookstoreItem.cs
@@ -0,0 +1,9 @@
+public class BookstoreItem
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+    public string? Category { get; set; }
+    public string? Author { get; set; }
+    public bool IsReady { get; set; }
+}
